0eaaf70898207e4ea3bd47bc54f31e818b0abfa0
Kevin Buley <kevin@buley.org>
2025-07-14 18:38:29 -0400

    import git aliases and worktree scripts update zsh git aliases and atuin add hosts to ssh configs

--- private_dot_config/zsh/zshrc.d/git.zsh
+++ private_dot_config/zsh/zshrc.d/git.zsh
@@ -1,2 +1,28 @@
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
 
+function git-top() {
+    root=$(git rev-parse --show-toplevel 2>/dev/null)
+    if [ -n "$root" ]; then
+        cd "$root"
+    else
+        echo "Not inside a Git repository."
+        return 1
+    fi
+}
+
+function git-root() {
+    git_root=$(pwd)
+    while [ "$git_root" != "/" ]; do
+        if [ -e "$git_root/.bare" ]; then
+            cd "$git_root"
+            return 0
+        fi
+        git_root=$(dirname "$git_root")
+    done
+    if [ "$git_root" == "/" ]; then
+        echo "You are not in a bare worktree."
+        return 1
+    fi
+}
+
+

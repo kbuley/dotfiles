08901b2a389c5059ff09674339dfe35ae9726942
Kevin Buley <kevin@buley.org>
2026-02-16 19:18:14 -0500

    change ssh controlmaster, GOBIN, and ignore invalid ssh options

--- private_dot_config/zsh/zshrc.d/go.zsh
+++ private_dot_config/zsh/zshrc.d/go.zsh
@@ -2,7 +2,7 @@ if [[ "$(uname)" = Darwin ]]; then
   export GOROOT="$(brew --prefix golang)/libexec"
 fi
 
-export GOPATH=${XDG_CONFIG_HOME}/bin
+export GOPATH=$HOME/go
 export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
 
 test -d "${GOPATH}" || mkdir "${GOPATH}"
